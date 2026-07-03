2c54ec03e655ce48fa483fa2047870835f23d6db
mohanasundaramp <mohanasundaramp@localhost>
2026-07-03 21:02:16 +0530

    DC

--- TestNGBasics/src/ParallelExecution/ParallelExecutionTest.java
+++ TestNGBasics/src/ParallelExecution/ParallelExecutionTest.java
@@ -10,7 +10,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class ParallelExecutionTest {
+public class ParallelExecutionTest 
+{
 	
 	
 	WebDriver driver;
